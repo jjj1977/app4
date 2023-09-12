3c4284e6c0e4c186817e0e9ba3dc2cca5d8587b2
JuniperEX2200 <jo22.button@gmail.com>
2023-09-12 18:04:55 +0300

    pdf 2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fpdf import FPDF
+import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.add_page()
-pdf.set_font(family="Times", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Hello!!!!!!!!!",
-         align="L", ln=1, border=1)
-pdf.cell(w=0, h=12, txt="Hiiiiiiiiiiiiiiiii",
-         align="L", ln=1, border=1)
+df = pd.read_csv("topics.csv")
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Times", style="B", size=23)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"],
+             align="L", ln=1, border=0)
+    pdf.line(10, 22, 200, 22)
 pdf.output("output.pdf")
